9433eb65fa2a0a048be8bd2535a6a1f41ca0c222
Fred Chien <cfsghost@gmail.com>
2012-05-20 21:43:11 +0800

    rename directory of example

--- examples/blog/app.js
+++ examples/blog/app.js
@@ -4,7 +4,7 @@ var port = 57788;
 var app = kamalan.createServer();
 
 app.configure(function() {
-	app.set('views', __dirname + '/render');
+	app.set('views', __dirname + '/views');
 	app.set('route', __dirname + '/route');
 	app.set('client', __dirname + '/client');
 	app.set('rudan', __dirname + '/rudan');
