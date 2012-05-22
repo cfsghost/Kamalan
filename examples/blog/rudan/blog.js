e9d9449e4ea521741055d6ba9bb165f33895929c
Fred Chien <cfsghost@gmail.com>
2012-05-22 12:25:48 +0800

    rework AST parser

--- examples/blog/rudan/blog.js
+++ examples/blog/rudan/blog.js
@@ -22,3 +22,29 @@ var Blog = new function() {
 		callback(null, articles.length);
 	};
 };
+
+function TestClass() {
+	var privateMember = 'Good';
+	this.testMethod1 = function() {
+	};
+	this.testMethod2 = function() {
+	};
+	this.testMember = 'Cool';
+}
+
+var TestFunc1 = function(param) {
+};
+
+function TestFunc2(param) {
+};
+
+var obj = {
+	A: 123,
+	B: 'Nice',
+	C: function() {
+	}
+};
+var arr = [];
+var num = 123;
+
+var TestClassInstance = new TestClass;
